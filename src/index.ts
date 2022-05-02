c7e79780699c33d27ee192b83627c6030ea8a5e9
mymatsubara <murilo.matsubara@gmail.com>
2022-05-02 17:35:38 -0300

    Initial graphql server setup

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { config } from "dotenv";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+// Make environment variables from `.env` available
+config();
+
+// Graphql schema
+const schema = buildSchema(`
+type Query {
+	hello: String
+}
+`);
+
+// Graphql handler
+const rootValue = {
+	hello: () => "Hello world!",
+};
+
+// Config and initialize server
+const PORT = process.env.APP_PORT || "4000";
+const app = express();
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		rootValue,
+		graphiql: true,
+	})
+);
+app.listen(PORT);
+console.log(`Graphql server is running on: http://localhost:${PORT}/graphql`);
