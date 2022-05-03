07fff1f8cb7812dd7ad392bff6fc33ad6cdd8102
mymatsubara <murilo.matsubara@gmail.com>
2022-05-03 09:54:01 -0300

    Fixed variable assignment

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,9 @@ async function main() {
   configAndInitilizeServer();
 }
 
-<<<<<<< HEAD
 async function configAndInitilizeServer() {
-  const PORT = process.env.APP_PORT || '4000';
-  const PATH = process.env.GRAPHQL_PATH || '/graphql';
-=======
-  // Config and initialize server
-  const PORT = process.env.APP_PORT || DEFAULT_SERVER_PORT;
-  const PATH = process.env.GRAPHQL_PATH || DEFAULT_GRAPHQL_PATH;
->>>>>>> Extracted project constants to a separate file
+  const PORT = process.env.APP_PORT ?? DEFAULT_SERVER_PORT;
+  const PATH = process.env.GRAPHQL_PATH ?? DEFAULT_GRAPHQL_PATH;
 
   await startApolloServer(helloTypeDef, helloResolver, PORT, PATH);
 
