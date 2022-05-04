4300a9291f9fdc257ee94f2b32b2efab398a3125
mymatsubara <murilo.matsubara@gmail.com>
2022-05-04 10:18:44 -0300

    Removed unnecessary constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,3 @@
 // Application constants
 export const DEFAULT_SERVER_PORT = 4000;
 export const DEFAULT_GRAPHQL_PATH = '/graphql';
-
-// Database contants
-export const DEFAULT_POSTGRES_HOST = 'localhost';
-export const DEFAULT_POSTGRES_PORT = '5432';
-export const DEFAULT_POSTGRES_USER = 'postgres';
-export const DEFAULT_POSTGRES_PASSWORD = 'password';
-export const DEFAULT_POSTGRES_DB = 'postgres';
