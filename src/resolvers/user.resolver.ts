6b6cc01ff1db73e68d8f7e58e17f354b3698a1fc
mymatsubara <murilo.matsubara@gmail.com>
2022-05-05 12:28:09 -0300

    Added duplicate email validation

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,4 +1,6 @@
 import { ValidationError } from 'apollo-server-core';
+import { QueryFailedError } from 'typeorm';
+import { UNIQUE_CONSTRAINT_ERROR_CODE } from '../consts';
 import { appDataSource } from '../data-source';
 import { User } from '../entities/user.entity';
 import { isPasswordValid } from '../utils/password';
@@ -6,19 +8,35 @@ import { isPasswordValid } from '../utils/password';
 export const userResolver = {
   Mutation: {
     createUser: async function (_: never, { user }: { user: User }) {
-      const { valid, brokenRules } = isPasswordValid(user.password);
-      if (!valid) {
-        throw new ValidationError(`Password: ${brokenRules}`);
+      validatePassword(user);
+
+      try {
+        const newUser = await appDataSource.manager.save(User, user);
+        return {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email,
+          birthDate: newUser.birthDate,
+        };
+      } catch (error) {
+        handleUserCreationError(error);
+        return undefined;
       }
-
-      const newUser = await appDataSource.manager.save(User, user);
-
-      return {
-        id: newUser.id,
-        name: newUser.name,
-        email: newUser.email,
-        birthDate: newUser.birthDate,
-      };
     },
   },
 };
+
+function validatePassword(user: User) {
+  const { valid, brokenRules } = isPasswordValid(user.password);
+  if (!valid) {
+    throw new ValidationError(`Password: ${brokenRules}`);
+  }
+}
+
+function handleUserCreationError(error: Error) {
+  if (error instanceof QueryFailedError && error.driverError.code == UNIQUE_CONSTRAINT_ERROR_CODE) {
+    throw new ValidationError('Email is already used.');
+  }
+
+  throw error;
+}
