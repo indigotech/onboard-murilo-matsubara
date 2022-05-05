dfdd159877cd1f1ee5cafd68e24ee27952c4313c
mymatsubara <murilo.matsubara@gmail.com>
2022-05-05 15:42:58 -0300

    Fixed data-source formating

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,26 +6,26 @@ import { isDevEnvironment } from './utils/env';
 makeDotenvAvailable();
 
 if (
-    !process.env.DB_HOST ||
-    !process.env.DB_PORT ||
-    !process.env.DB_USER ||
-    !process.env.DB_PASSWORD ||
-    !process.env.DB_DATABASE
+  !process.env.DB_HOST ||
+  !process.env.DB_PORT ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_DATABASE
 ) {
-    throw ReferenceError(
-        'Could not find one of the required variables inside of `.env`. Check `README.md` for more instructions.',
-    );
+  throw ReferenceError(
+    'Could not find one of the required variables inside of `.env`. Check `README.md` for more instructions.',
+  );
 }
 
 export const appDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    synchronize: isDevEnvironment(),
-    entities: ['dist/**/*.entity.js'],
-    migrations: [],
-    subscribers: [],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: isDevEnvironment(),
+  entities: ['dist/**/*.entity.js'],
+  migrations: [],
+  subscribers: [],
 });
