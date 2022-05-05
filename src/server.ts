9a5eb9b81913faf747028f6f5b3a2ed0d302fdc1
mymatsubara <murilo.matsubara@gmail.com>
2022-05-05 15:44:26 -0300

    Removed GraphqlContext interface

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,6 @@ import { ApolloServerPluginDrainHttpServer, Config } from 'apollo-server-core';
 import { ApolloServer, ExpressContext } from 'apollo-server-express';
 import express from 'express';
 import http from 'http';
-import { DataSource } from 'typeorm';
-
-export interface GraphqlContext {
-  dataSource: DataSource;
-}
 
 export async function startApolloServer(
   typeDefs: Config<ExpressContext>['typeDefs'],
