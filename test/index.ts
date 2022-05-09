99ec34049b244ff647378d42968e80f4a9a25e0f
mymatsubara <murilo.matsubara@gmail.com>
2022-05-09 16:14:21 -0300

    Fixed and refactored tests

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
 import { expect } from 'chai';
 import { config as makeDotenvAvailable } from 'dotenv';
 import { Server } from 'http';
 import path from 'path';
+import { getDataSource } from '../src/data-source';
+import { User } from '../src/entities/user.entity';
 import { setupServer } from '../src/server';
 import { Env } from '../src/utils/env';
+import { makeGraphqlResquest } from '../src/utils/graphql';
+import { checkPassword } from '../src/utils/password';
 
 let server: Server;
 let TEST_SERVER_URL: string;
@@ -19,20 +22,11 @@ describe('Hello query', () => {
   it('must return hello message', async () => {
     const expectedResponseData = { data: { hello: 'Hello world!' } };
 
-    const response = await axios.post(
+    const response = await makeGraphqlResquest(
       TEST_SERVER_URL,
-      {
-        query: `
-        query hello {
-          hello
-        }`,
-        variables: {},
-      },
-      {
-        headers: {
-          'content-type': 'application/json',
-        },
-      },
+      `query hello {
+        hello
+      }`,
     );
 
     expect(response.data).to.be.deep.equal(expectedResponseData);
@@ -40,48 +34,57 @@ describe('Hello query', () => {
 });
 
 describe('CreateUser mutation', () => {
+  beforeEach(async () => {
+    await getDataSource().manager.clear(User);
+  });
+
   it('must create user sucessfully', async () => {
-    const userToCreate = {
+    const newUser = {
       name: 'Test',
       email: 'test@test.com',
-      birthDate: '01/01/2000',
-    };
-    const expectedResponseData = {
-      data: {
-        createUser: {
-          id: 1,
-          ...userToCreate,
-        },
-      },
+      birthDate: '2000-01-01',
+      password: 'test12',
     };
 
-    const response = await axios.post(
+    const mutationResponse = await makeGraphqlResquest(
       TEST_SERVER_URL,
+      `mutation CreateUser($user: UserInput) {
+        createUser(user: $user) {
+          id
+          name
+          email
+          birthDate
+        }
+      }`,
       {
-        query: `
-        mutation CreateUser($user: UserInput) {
-          createUser(user: $user) {
-            id
-            name
-            email
-            birthDate
-          }
-        }`,
-        variables: {
-          user: {
-            ...userToCreate,
-            password: 'test12',
-          },
-        },
-      },
-      {
-        headers: {
-          'content-type': 'application/json',
-        },
+        user: newUser,
       },
     );
+    const dbCreatedUser = await getDataSource().manager.findOne(User, { where: { id: mutationResponse.data.id } });
+    const matchPassword = await checkPassword(newUser.password, dbCreatedUser?.password ?? '');
 
-    expect(response.data).to.be.deep.equal(expectedResponseData);
+    expect(dbCreatedUser).to.not.be.undefined;
+    expect(mutationResponse.data).to.be.deep.equal({
+      data: {
+        createUser: {
+          name: newUser.name,
+          email: newUser.email,
+          birthDate: newUser.birthDate,
+          id: dbCreatedUser?.id,
+        },
+      },
+    });
+    expect({
+      name: dbCreatedUser?.name,
+      email: dbCreatedUser?.email,
+    }).to.be.deep.equal({
+      name: newUser.name,
+      email: newUser.email,
+    });
+    expect(new Date(dbCreatedUser?.birthDate ?? '').toDateString()).to.be.equals(
+      new Date(newUser.birthDate).toDateString(),
+    );
+    expect(matchPassword).to.be.equal(true);
   });
 });
 
