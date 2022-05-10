925a85e0513dd6ed72b33a624adb3f5114bdf6d8
mymatsubara <murilo.matsubara@gmail.com>
2022-05-10 16:09:15 -0300

    Changed additionalInfo type

--- src/exceptions/custom-error.ts
+++ src/exceptions/custom-error.ts
@@ -2,8 +2,7 @@ export class CustomError extends Error {
   constructor(
     public code: number,
     public message: string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    public additionalInfo?: string | Record<string, any>,
+    public additionalInfo?: string | object,
     public name: string = 'CustomError',
   ) {
     super(message);
