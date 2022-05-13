19feacc7bc0412dbeb2e8ee2c5edead975cd59aa
mymatsubara <murilo.matsubara@gmail.com>
2022-05-13 16:48:24 -0300

    Removed unnused queries

--- src/queries/paginated-users.sql
+++
@@ -1,4 +0,0 @@
-select id, name, email, "birthDate"
-from public.user
-order by name, id asc
-limit $1 offset $2;
--- src/queries/queries.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs';
-
-const basePath = process.cwd() + '/src/queries';
-
-export const Queries = {
-  paginatedUsers: readFileSync(basePath + '/paginated-users.sql').toString(),
-  paginatedUsersFilterId: readFileSync(basePath + '/paginated-users-filter-id.sql').toString(),
-};
