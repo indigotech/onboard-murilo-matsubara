15103a65ace01296c95c7b92dba5ba467702e68b
mymatsubara <murilo.matsubara@gmail.com>
2022-05-13 16:46:15 -0300

    Added option to seed users with addresses

--- src/seeds/users.ts
+++ src/seeds/users.ts
@@ -1,11 +1,18 @@
 import { faker } from '@faker-js/faker';
 import { DataSource } from 'typeorm';
+import { Address } from '../entities/address.entity';
 import { User } from '../entities/user.entity';
 import { toDateString } from '../utils/date';
 import { hashPassword } from '../utils/password';
 
-export async function seedUsers(userCount: number, dataSource: DataSource) {
+export async function seedUsers(userCount: number, dataSource: DataSource, withAddresses = 1) {
   const newUsers = await createRandomUsers(userCount);
+
+  if (withAddresses > 0) {
+    for (const newUser of newUsers) {
+      newUser.addresses = createRandomAddresses(withAddresses);
+    }
+  }
 
   return await dataSource.manager.save(User, newUsers);
 }
@@ -14,7 +21,7 @@ async function createRandomUsers(number: number) {
   return Promise.all(Array.from({ length: number }, () => randomUser()));
 }
 
-async function randomUser(): Promise<User> {
+export async function randomUser(): Promise<User> {
   const user = new User();
   user.name = faker.name.findName();
   user.email = faker.internet.email();
@@ -22,4 +29,21 @@ async function randomUser(): Promise<User> {
   user.password = await hashPassword(faker.internet.password());
 
   return user;
+}
+
+function createRandomAddresses(number: number) {
+  return Array.from({ length: number }, () => randomAddress());
+}
+
+export function randomAddress(): Address {
+  const address = new Address();
+  address.city = faker.address.cityName();
+  address.state = faker.address.state();
+  address.street = faker.address.streetName();
+  address.streetNumber = parseInt(faker.address.buildingNumber());
+  address.complement = faker.address.secondaryAddress();
+  address.neighborhood = faker.address.county();
+  address.postalCode = faker.address.zipCode();
+
+  return address;
 }
